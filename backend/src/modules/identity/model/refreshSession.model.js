4807aea965059a5cf45de8b76c50220285c8fc1a
Purushottam Nagar <purushottamnagar27082006@gmail.com>
2026-08-31 02:25:19 +0530

    feat: enhance user authentication with password hashing and JWT token generation; add refresh session model

---
+++ backend/src/modules/identity/model/refreshSession.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const refreshSessionSchema  = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true, // index: true, because refresh operations will frequently need to find sessions belonging to a user.
+    },
+    tokenHash:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+        index: true
+    },
+    revokedAt: {
+        type: Date,
+        default: null
+    },
+    tokenFamily: {
+        type: String,
+        required: true,
+        index: true
+    }
+
+},{timestamps:true})
+
+export const RefreshSession = mongoose.model('RefreshSession',refreshSessionSchema )
--- backend/src/modules/identity/model/user.model.js
+++ backend/src/modules/identity/model/user.model.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { randomUUID } from "crypto";
+import jwt from 'jsonwebtoken'
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -40,16 +44,35 @@ const userSchema = new mongoose.Schema({
 export const User = mongoose.model('User', userSchema)
 
 
+userSchema.pre("save", async function () { // there we have to use normal function not arrow to use this property
+  // hook pre is used document ke save hone ke just phele hash kro
+  if (!this.isModified("password")) return ; // passwrod change hua tabhi hash kro bar bar ni
 
-// ACTIVE
-//     ↓
-// normal authentication/access
+  this.password = await bcrypt.hash(this.password, 12);
+  
+});
 
-// SUSPENDED
-//     ↓
-// login/access denied
+userSchema.methods.isPasswordCorrect = async function (password){  // method to check the password
+  return await bcrypt.compare(password,this.password) 
+}
 
-// DEACTIVATED
-//     ↓
-// login/access denied
+userSchema.methods.generateAccessToken = function(){ // these is the method we have injected inside the user
+  const payload = {
+    sub:this._id.toString(),
+    role:this.role
+  }
+  const secret = process.env.JWT_ACCESS_SECRET
+  const expiry = process.env.JWT_ACCESS_EXPIRY
 
+  return jwt.sign(payload,secret,{expiresIn:expiry})
+}
+userSchema.methods.generateRefreshToken = function(){
+  const payload = {    // it contain less info as it got refresh and its duration is more than access
+    sub:this._id.toString(),
+    jti: randomUUID() // 
+  }
+  const secret = process.env.JWT_REFRESH_SECRET
+  const expiry = process.env.JWT_REFRESH_EXPIRY
+  
+  return jwt.sign(payload,secret,{expiresIn:expiry})
+}
